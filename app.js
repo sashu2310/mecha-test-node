60dce679c20883ec4435a674bc8af894e94e010d
sashu2310 <sandeepkesar2310@gmail.com>
2020-01-30 13:16:15 +0530

    env fixes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const routes = require('./router/indexRoute');
 
 // port number
-const port = 3000;
+const port = process.env.port;
 //database url
 const dbUrl = process.env.dbUrl;
 //connecting database
